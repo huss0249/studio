3686583ef31111154f87374641d4bbe6ca80e257
Ahmed Hussein <huss0249@algonquinlive.com>
2021-01-20 08:45:03 -0500

    duplicated the data

---
+++ items-data.js
@@ -0,0 +1,101 @@
+const items-data =[
+  {
+    title: 'modified Blue T-Shirt',
+    image: 'http://algonquinlivecom-my.sharepoint.com/personal/huss0249_algonquinlive_com/Documents/test-web-img/IMG_1813.JPG',
+    price: 7.99,
+    stock: 4,
+    nextShipment: {
+      day: 1,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'second Bright Purple T-Shirt',
+    image: 'http://algonquinlivecom-my.sharepoint.com/personal/huss0249_algonquinlive_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fhuss0249%5Falgonquinlive%5Fcom%2FDocuments%2Ftest%2Dweb%2Dimg%2FIMG%5F1813%2EJPG&parent=%2Fpersonal%2Fhuss0249%5Falgonquinlive%5Fcom%2FDocuments%2Ftest%2Dweb%2Dimg&originalPath=aHR0cHM6Ly9hbGdvbnF1aW5saXZlY29tLW15LnNoYXJlcG9pbnQuY29tLzppOi9nL3BlcnNvbmFsL2h1c3MwMjQ5X2FsZ29ucXVpbmxpdmVfY29tL0VRSnh1Z1NZX2hsRHVPVTBINDRESWVFQnU2cnMtcEhuVXNUQXN4cG9FMTJBbGc_cnRpbWU9UGhnSzc2SzgyRWc',
+    price: 5.99,
+    stock: 1,
+    nextShipment: {
+      day: 15,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'g1Cobalt Blue T-Shirt',
+    image: 'http://drive.google.com/drive/folders/1hYRIqnD4r_qGZedc4bxC90G6nntabptt?usp=sharing/1.png',
+    price: 9.99,
+    stock: 5,
+    nextShipment: {
+      day: 3,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'g2Green T-Shirt',
+    image: 'http://drive.google.com/drive/folders/1hYRIqnD4r_qGZedc4bxC90G6nntabptt?usp=sharing/2.png',
+    price: 6.99,
+    stock: 0,
+    nextShipment: {
+      day: 1,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'ggGrey T-Shirt',
+    image: 'http://drive.google.com/drive/folders/1hYRIqnD4r_qGZedc4bxC90G6nntabptt?usp=sharing',
+    price: 4.99,
+    stock: 2,
+    nextShipment: {
+      day: 10,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'wwwgLight Green T-Shirt',
+    image: 'http://drive.google.com/file/d/1m9OseQUdLqhH9dgMF3asYFSZelK2GC1s/view?usp=sharing',
+    price: 7.99,
+    stock: 4,
+    nextShipment: {
+      day: 3,
+      month: 7,
+      year: 2020
+    }
+  },
+  {
+    title: 'Purple T-Shirt',
+    image: 'images/purple-t-shirt.jpg',
+    price: 7.99,
+    stock: 0,
+    nextShipment: {
+      day: 1,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Red T-Shirt',
+    image: 'red-t-shirt.jpg',
+    price: 6.99,
+    stock: 3,
+    nextShipment: {
+      day: 8,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Teal T-Shirt',
+    image: 'teal-t-shirt.jpg',
+    price: 7.99,
+    stock: 2,
+    nextShipment: {
+      day: 1,
+      month: 7,
+      year: 2020
+    }
+  }
+]
